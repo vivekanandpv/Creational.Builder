ae9ce8d1e68e18e8be226d310e302a55343d7ade
Vivekanand P V <vivekanandpv@outlook.com>
2020-12-16 12:05:44 +0530

    Add project files.

---
+++ Lib.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Creational.Builder
+{
+    //  GoF: Separate the construction of a complex object from its
+    //  representation so that the same construction process can
+    //  create different representations.
+
+    //  Constructors can be too long and tedious
+    //  Make the process of object creation intuitive
+
+    public interface IServerBuilder
+    {
+        IServerBuilder AddIpAddress(string ipAddress);
+        IServerBuilder AddLocation(string location);
+        IServerBuilder AddOperatingSystem(string operatingSystem);
+        IServerBuilder AddBlockCode(int blockCode);
+        IServerBuilder AddAvailabilityZone(string availabilityZone);
+        Server Build();
+    }
+
+    public class Server
+    {
+        public string IpAddress { get; set; }
+        public string Location { get; set; }
+        public string OperatingSystem { get; set; }
+        public int BlockCode { get; set; }
+        public string AvailabilityZone { get; set; }
+    }
+
+    public class ServerBuilder : IServerBuilder
+    {
+        private Server _server;
+
+        public ServerBuilder()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            this._server = new Server();
+        }
+        public IServerBuilder AddIpAddress(string ipAddress)
+        {
+            this._server.IpAddress = ipAddress;
+            return this;
+        }
+
+        public IServerBuilder AddLocation(string location)
+        {
+            this._server.Location = location;
+            return this;
+        }
+
+        public IServerBuilder AddOperatingSystem(string operatingSystem)
+        {
+            this._server.OperatingSystem = operatingSystem;
+            return this;
+        }
+
+        public IServerBuilder AddBlockCode(int blockCode)
+        {
+            this._server.BlockCode = blockCode;
+            return this;
+        }
+
+        public IServerBuilder AddAvailabilityZone(string availabilityZone)
+        {
+            this._server.AvailabilityZone = availabilityZone;
+            return this;
+        }
+
+        public Server Build()
+        {
+            return this._server;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Creational.Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServerBuilder serverBuilder = new ServerBuilder();
+            var server = 
+                serverBuilder
+                .AddIpAddress("127.0.0.1")
+                .AddOperatingSystem("Windows Server 2012")
+                .AddAvailabilityZone("India-1")
+                .AddLocation("Mumbai")
+                .AddBlockCode(123)
+                .Build();
+        }
+    }
+}
